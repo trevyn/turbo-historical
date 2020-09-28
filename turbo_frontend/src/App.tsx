0296f81359e73460d870313ffd804e5ea0bf2719
Eden <github@eden2.com>
2020-09-28 10:38:51 +0400

    comment out old navitem

--- turbo_frontend/src/App.tsx
+++ turbo_frontend/src/App.tsx
@@ -516,20 +516,20 @@ const ResultsComponent: FC<{ results: codegen.ResultItem[] }> = ({ results }) =>
 //  );
 // };
 
-const NavItem: FC<{ active: boolean; onClick: Function }> = ({ active, onClick, children }) => {
- return (
-  <div
-   onClick={() => onClick()}
-   className={`cursor-pointer mx-3 inline-flex items-center px-2 pt-1 border-b-2 text-sm font-medium leading-5 focus:outline-none transition duration-150 ease-in-out ${
-    active
-     ? "border-indigo-500 text-gray-900 focus:border-indigo-700"
-     : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300"
-   }`}
-  >
-   {children}
-  </div>
- );
-};
+// const NavItem: FC<{ active: boolean; onClick: Function }> = ({ active, onClick, children }) => {
+//  return (
+//   <div
+//    onClick={() => onClick()}
+//    className={`cursor-pointer mx-3 inline-flex items-center px-2 pt-1 border-b-2 text-sm font-medium leading-5 focus:outline-none transition duration-150 ease-in-out ${
+//     active
+//      ? "border-indigo-500 text-gray-900 focus:border-indigo-700"
+//      : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300"
+//    }`}
+//   >
+//    {children}
+//   </div>
+//  );
+// };
 
 // const SetSearchResult = React.createContext<React.Dispatch<Array<codegen.ResultItem>> | null>(null);
 
