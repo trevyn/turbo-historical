48f619beb3052b48e8bd489cec2e289626af0d0e
Eden <github@eden2.com>
2020-12-03 19:47:55 +0400

    add a bunch of tests for turbosql select! macro

--- turbosql/tests/integration_test.rs
+++ turbosql/tests/integration_test.rs
@@ -1,6 +1,6 @@
 use turbosql::{select, Blob, Turbosql};
 
-#[derive(Turbosql, Default, Debug)]
+#[derive(Turbosql, Default, Debug, PartialEq, Clone)]
 struct PersonIntegrationTest {
  rowid: Option<i64>,
  name: Option<String>,
@@ -8,17 +8,43 @@ struct PersonIntegrationTest {
  image_jpg: Option<Blob>,
 }
 
-// @test integration test
+/* @test integration test
+cd turbosql
+cargo test --features test -- --nocapture
+*/
 #[test]
 fn it_works() {
- PersonIntegrationTest {
+ let mut row = PersonIntegrationTest {
   rowid: None,
   name: Some("Bob".to_string()),
   age: Some(42),
   image_jpg: None,
- }
- .insert()
- .unwrap();
+ };
 
- println!("{:#?}", select!(Vec<PersonIntegrationTest>));
+ row.insert().unwrap();
+
+ row.rowid = Some(1);
+
+ assert!(select!(i64 "1").unwrap() == 1);
+ assert!(select!(i64 "SELECT 1").unwrap() == 1);
+ // assert!(select!(Option<i64> "SELECT 1").unwrap() == Some(1));
+
+ assert!(select!(PersonIntegrationTest).unwrap() == row);
+ assert!(select!(Vec<PersonIntegrationTest>).unwrap() == vec![row.clone()]);
+ assert!(select!(Option<PersonIntegrationTest>).unwrap() == Some(row.clone()));
+
+ assert!(select!(PersonIntegrationTest "WHERE age = ?", row.age).unwrap() == row);
+ assert!(
+  select!(Vec<PersonIntegrationTest> "WHERE age = ?", row.age).unwrap() == vec![row.clone()]
+ );
+ assert!(
+  select!(Option<PersonIntegrationTest> "WHERE age = ?", row.age).unwrap() == Some(row.clone())
+ );
+
+ assert!(select!(PersonIntegrationTest "WHERE age = 41").is_err());
+ assert!(select!(Vec<PersonIntegrationTest> "WHERE age = 41").unwrap() == vec![]);
+ assert!(select!(Option<PersonIntegrationTest> "WHERE age = 41").unwrap() == None);
+
+ assert!(select!(i64 "age FROM personintegrationtest").unwrap() == row.age.unwrap());
+ assert!(select!(i64 "SELECT age FROM personintegrationtest").unwrap() == row.age.unwrap());
 }
